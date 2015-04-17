5b67bf19c9be048e2af5692bfb72e1ea51ca3468
keithlei <keithlei01@gmail.com>
2015-04-17 14:45:02 +0800

    post android flag

--- android-studio/MyApplication/app/src/main/java/miniBean/viewmodel/NewPost.java
+++ android-studio/MyApplication/app/src/main/java/miniBean/viewmodel/NewPost.java
@@ -4,12 +4,14 @@ public class NewPost {
     public Long community_id;
     public String questionTitle,questionText;
     boolean withPhotos;
+    boolean android;
 
     public NewPost(Long community_id, String questionTitle,String questionText, boolean withPhotos) {
         this.community_id = community_id;
         this.questionTitle = questionTitle;
         this.questionText=questionText;
         this.withPhotos = withPhotos;
+        this.android = true;
     }
 
 }
