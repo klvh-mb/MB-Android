77e244cbcd5df30d3b10d7160ab8c92fc14c5889
keithlei <keithlei01@gmail.com>
2015-05-12 15:40:34 +0800

    pn comm header

--- android-studio/MyApplication/app/src/main/java/miniBean/fragment/PNCommunityFragment.java
+++ android-studio/MyApplication/app/src/main/java/miniBean/fragment/PNCommunityFragment.java
@@ -5,12 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.ScrollView;
 import android.widget.TextView;
 
 import org.parceler.apache.commons.lang.StringUtils;
@@ -39,13 +39,13 @@ public class PNCommunityFragment extends MyFragment {
     private TextView nameText,districtText,enNameText,orgValue,typeValue,timeValue,curriculumValue;
     private TextView studentNum,halfDayValue,fullDayValue,curriculumContent,addressText,phoneValue;
     private TextView websiteValue,postCount;
-    private ScrollView scrollView;
     private String govtUrlValue;
 
     private NewsfeedListAdapter feedListAdapter;
     private List<CommunityPostVM> feedItems;
 
-    private ListView postList;
+    private View listHeader, loadingFooter;
+    private ListView listView;
 
     private ImageView icon,couponImage,govtImage;
     private LinearLayout gotoCommLayout,newPostLayout;
@@ -58,38 +58,50 @@ public class PNCommunityFragment extends MyFragment {
 
         View view = inflater.inflate(R.layout.pn_community_fragment, container, false);
 
-        icon = (ImageView) view.findViewById(R.id.schoolImage);
-        nameText = (TextView) view.findViewById(R.id.schoolNameText);
-        districtText = (TextView) view.findViewById(R.id.distNameText);
-        enNameText = (TextView) view.findViewById(R.id.enName);
-        couponImage = (ImageView) view.findViewById(R.id.couponImage);
-        orgValue = (TextView) view.findViewById(R.id.orgValueText);
-        timeValue = (TextView) view.findViewById(R.id.timeValueText);
-        typeValue = (TextView) view.findViewById(R.id.typeValueText);
-        curriculumValue = (TextView) view.findViewById(R.id.curriValueText);
-        studentNum = (TextView) view.findViewById(R.id.studentValue);
-        halfDayValue = (TextView) view.findViewById(R.id.halfDayValue);
-        fullDayValue = (TextView) view.findViewById(R.id.fullDayValue);
-        curriculumContent = (TextView) view.findViewById(R.id.contentText);
-        addressText = (TextView) view.findViewById(R.id.addressValue);
-        phoneValue = (TextView) view.findViewById(R.id.phoneValue);
-        websiteValue = (TextView) view.findViewById(R.id.websiteValue);
-        newPostLayout = (LinearLayout) view.findViewById(R.id.newPostLayout);
-        postList = (ListView) view.findViewById(R.id.postList);
-        postCount = (TextView) view.findViewById(R.id.postCount);
-        gotoCommLayout = (LinearLayout) view.findViewById(R.id.gotoCommLayout);
-        scrollView = (ScrollView) view.findViewById(R.id.scrollview);
-        govtImage = (ImageView) view.findViewById(R.id.govtImage);
+        // header
+        listHeader = inflater.inflate(R.layout.pn_community_fragment_header, null);
+
+        // footer
+        loadingFooter = inflater.inflate(R.layout.list_loading_footer, null);
+
+        icon = (ImageView) listHeader.findViewById(R.id.schoolImage);
+        nameText = (TextView) listHeader.findViewById(R.id.schoolNameText);
+        districtText = (TextView) listHeader.findViewById(R.id.distNameText);
+        enNameText = (TextView) listHeader.findViewById(R.id.enName);
+        couponImage = (ImageView) listHeader.findViewById(R.id.couponImage);
+        orgValue = (TextView) listHeader.findViewById(R.id.orgValueText);
+        timeValue = (TextView) listHeader.findViewById(R.id.timeValueText);
+        typeValue = (TextView) listHeader.findViewById(R.id.typeValueText);
+        curriculumValue = (TextView) listHeader.findViewById(R.id.curriValueText);
+        studentNum = (TextView) listHeader.findViewById(R.id.studentValue);
+        halfDayValue = (TextView) listHeader.findViewById(R.id.halfDayValue);
+        fullDayValue = (TextView) listHeader.findViewById(R.id.fullDayValue);
+        curriculumContent = (TextView) listHeader.findViewById(R.id.contentText);
+        addressText = (TextView) listHeader.findViewById(R.id.addressValue);
+        phoneValue = (TextView) listHeader.findViewById(R.id.phoneValue);
+        websiteValue = (TextView) listHeader.findViewById(R.id.websiteValue);
+        newPostLayout = (LinearLayout) listHeader.findViewById(R.id.newPostLayout);
+        postCount = (TextView) listHeader.findViewById(R.id.postCount);
+        gotoCommLayout = (LinearLayout) listHeader.findViewById(R.id.gotoCommLayout);
+        govtImage = (ImageView) listHeader.findViewById(R.id.govtImage);
+
+        // list
+        listView = (ListView) view.findViewById(R.id.postList);
+        listView.addHeaderView(listHeader);
+        listView.addFooterView(loadingFooter);      // need to add footer before set adapter
 
         initInfo();
 
         getNewsFeedByCommunityId(getArguments().getLong("commId"));
 
-        feedItems = new ArrayList<CommunityPostVM>();
+        feedItems = new ArrayList<>();
         feedListAdapter = new NewsfeedListAdapter(getActivity(), feedItems, false);
-        postList.setAdapter(feedListAdapter);
+        listView.setAdapter(feedListAdapter);
 
-        postList.setOnTouchListener(new View.OnTouchListener() {
+        listView.setFriction(ViewConfiguration.getScrollFriction() *
+                DefaultValues.LISTVIEW_SCROLL_FRICTION_SCALE_FACTOR);
+
+        listView.setOnTouchListener(new View.OnTouchListener() {
             // Setting on Touch Listener for handling the touch inside ScrollView
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -99,11 +111,17 @@ public class PNCommunityFragment extends MyFragment {
             }
         });
 
-        postList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int headerViewsCount = listView.getHeaderViewsCount();
+                if (position < headerViewsCount) {
+                    // listview header
+                    return;
+                }
+
                 Intent intent = new Intent(getActivity(), DetailActivity.class);
-                CommunityPostVM post = (CommunityPostVM) feedListAdapter.getItem(position);
+                CommunityPostVM post = feedListAdapter.getItem(position - headerViewsCount);
                 if (post != null) {
                     intent.putExtra("postId", post.getId());
                     intent.putExtra("commId", post.getCid());
@@ -115,14 +133,15 @@ public class PNCommunityFragment extends MyFragment {
             }
         });
 
-        postList.setOnScrollListener(new InfiniteScrollListener(
-                DefaultValues.DEFAULT_INFINITE_SCROLL_VISIBLE_THRESHOLD, false, true) {
+        listView.setOnScrollListener(new InfiniteScrollListener(
+                DefaultValues.DEFAULT_INFINITE_SCROLL_VISIBLE_THRESHOLD, true, true) {
             @Override
             public void onLoadMore(int page, int totalItemsCount) {
+                loadingFooter.setVisibility(View.VISIBLE);
                 loadNewsfeed(
                         getArguments().getLong("id"),
-                        feedItems.get(feedItems.size() - 1).getUt() + "",       // NOTE: use updateTime not createTime!!
-                        page - 1);
+                        feedItems.get(feedItems.size()-1).getUt() + "",       // NOTE: use updateTime not createTime!!
+                        page-1);
             }
         });
 
@@ -139,7 +158,7 @@ public class PNCommunityFragment extends MyFragment {
         gotoCommLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                scrollView.fullScroll(View.FOCUS_DOWN);
+                listView.smoothScrollToPosition(1);
             }
         });
 
@@ -214,13 +233,13 @@ public class PNCommunityFragment extends MyFragment {
             couponImage.setImageResource(R.drawable.value_no);
         }
 
-        scrollView.post(new Runnable() {
+        listView.post(new Runnable() {
             @Override
             public void run() {
                 if ("FromCommentImage".equals(getArguments().getString("flag"))) {
-                    scrollView.fullScroll(View.FOCUS_DOWN);
+                    //listView.smoothScrollToPosition(1);
                 } else {
-                    scrollView.fullScroll(View.FOCUS_UP);
+                    listView.scrollTo(0,0);
                 }
             }
         });
@@ -241,10 +260,16 @@ public class PNCommunityFragment extends MyFragment {
         });
     }
 
-    public void loadNewsfeed(Long id, String date, int offset) {
+    private void loadNewsfeed(Long id, String date, int offset) {
         AppController.getApi().getCommunityNextPosts(id, date, AppController.getInstance().getSessionId(), new Callback<List<CommunityPostVM>>() {
             @Override
             public void success(List<CommunityPostVM> communityPostVMs, Response response) {
+                if (communityPostVMs == null || communityPostVMs.size() == 0) {
+                    setFooterText(R.string.list_loaded_all);
+                } else {
+                    setFooterText(R.string.list_loading);
+                }
+
                 feedItems.addAll(communityPostVMs);
                 feedListAdapter.notifyDataSetChanged();
             }
@@ -254,6 +279,11 @@ public class PNCommunityFragment extends MyFragment {
                 error.printStackTrace();
             }
         });
+    }
+
+    private void setFooterText(int text) {
+        TextView footerText = (TextView) listView.findViewById(R.id.listLoadingFooterText);
+        footerText.setText(text);
     }
 
     public void setSchool(PreNurseryVM schoolVM) {
