d344472117a8be7d30ea40a82a479b5c703c18a6
keithlei <keithlei01@gmail.com>
2015-05-21 16:50:12 +0800

    top schools layout

--- android-studio/MyApplication/app/src/main/java/miniBean/activity/DetailActivity.java
+++ android-studio/MyApplication/app/src/main/java/miniBean/activity/DetailActivity.java
@@ -820,15 +820,16 @@ public class DetailActivity extends FragmentActivity {
             e.printStackTrace();
         }
     }
+
     @Override
     public void onStart() {
         super.onStart();
         EasyTracker.getInstance(this).activityStart(this);
     }
+
     @Override
     public void onStop() {
         super.onStop();
         EasyTracker.getInstance(this).activityStop(this);
     }
-
 }
--- android-studio/MyApplication/app/src/main/java/miniBean/activity/TopSchoolsActivity.java
+++ android-studio/MyApplication/app/src/main/java/miniBean/activity/TopSchoolsActivity.java
@@ -10,8 +10,11 @@ import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
+
+import com.google.analytics.tracking.android.EasyTracker;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -53,14 +56,23 @@ public class TopSchoolsActivity extends FragmentActivity {
         getActionBar().setCustomView(R.layout.top_schools_actionbar);
 
         TextView actionbarTitle = (TextView) findViewById(R.id.title);
+        RelativeLayout topViewedLayout = (RelativeLayout) findViewById(R.id.topViewedLayout);
+        RelativeLayout topBookmarkedLayout = (RelativeLayout) findViewById(R.id.topBookmarkedLayout);
 
         if (getIntent().getStringExtra("flag").equals(SchoolsPNFragment.INTENT_FLAG)) {
             getActionBar().setBackgroundDrawable(getResources().getDrawable(R.drawable.actionbar_bg_green));
             actionbarTitle.setText(getString(R.string.schools_top_pn_ranking));
+            topViewedLayout.setBackgroundResource(R.drawable.schools_pn_edit_text_round);
+            topBookmarkedLayout.setBackgroundResource(R.drawable.schools_pn_edit_text_round);
         } else if (getIntent().getStringExtra("flag").equals(SchoolsKGFragment.INTENT_FLAG)) {
             getActionBar().setBackgroundDrawable(getResources().getDrawable(R.drawable.actionbar_bg_maroon));
             actionbarTitle.setText(getString(R.string.schools_top_kg_ranking));
+            topViewedLayout.setBackgroundResource(R.drawable.schools_kg_edit_text_round);
+            topBookmarkedLayout.setBackgroundResource(R.drawable.schools_kg_edit_text_round);
         }
+
+        //topViewedLayout.setPadding(10,10,10,10);
+        //topBookmarkedLayout.setPadding(10,10,10,10);
 
         topViewedPNs = new ArrayList<>();
         topBookmarkedPNs = new ArrayList<>();
@@ -139,7 +151,7 @@ public class TopSchoolsActivity extends FragmentActivity {
                 topViewedPNs.addAll(vms);
                 topViewedPNListAdapter = new TopViewedPNListAdapter(TopSchoolsActivity.this,topViewedPNs);
                 topViewedList.setAdapter(topViewedPNListAdapter);
-               setListViewHeightBasedOnChildren(topViewedList);
+                setListViewHeightBasedOnChildren(topViewedList);
                 scrollView.fullScroll(View.FOCUS_UP);
             }
 
@@ -151,11 +163,11 @@ public class TopSchoolsActivity extends FragmentActivity {
     }
 
     private void getTopBookmarkPNs(){
-        AppController.getApi().getTopBookmarkedPNs(AppController.getInstance().getSessionId(),new Callback<List<PreNurseryVM>>() {
+        AppController.getApi().getTopBookmarkedPNs(AppController.getInstance().getSessionId(), new Callback<List<PreNurseryVM>>() {
             @Override
             public void success(List<PreNurseryVM> vms, Response response) {
                 topBookmarkedPNs.addAll(vms);
-                topBookmarkedPNListAdapter = new TopBookmarkedPNListAdapter(TopSchoolsActivity.this,topBookmarkedPNs);
+                topBookmarkedPNListAdapter = new TopBookmarkedPNListAdapter(TopSchoolsActivity.this, topBookmarkedPNs);
                 topBookmarkedList.setAdapter(topBookmarkedPNListAdapter);
                 setListViewHeightBasedOnChildren(topBookmarkedList);
                 scrollView.fullScroll(View.FOCUS_UP);
@@ -163,18 +175,19 @@ public class TopSchoolsActivity extends FragmentActivity {
 
             @Override
             public void failure(RetrofitError error) {
-                   error.printStackTrace();
+                error.printStackTrace();
             }
         });
     }
 
     private void getTopViewsKGs(){
-        AppController.getApi().getTopViewedKGs(AppController.getInstance().getSessionId(),new Callback<List<KindergartenVM>>() {
+        AppController.getApi().getTopViewedKGs(AppController.getInstance().getSessionId(), new Callback<List<KindergartenVM>>() {
             @Override
             public void success(List<KindergartenVM> vms, Response response) {
                 topViewedKGs.addAll(vms);
-                topViewedKGListAdapter = new TopViewedKGListAdapter(TopSchoolsActivity.this,topViewedKGs);
+                topViewedKGListAdapter = new TopViewedKGListAdapter(TopSchoolsActivity.this, topViewedKGs);
                 topViewedList.setAdapter(topViewedKGListAdapter);
+                setListViewHeightBasedOnChildren(topViewedList);
                 scrollView.fullScroll(View.FOCUS_UP);
             }
 
@@ -186,12 +199,13 @@ public class TopSchoolsActivity extends FragmentActivity {
     }
 
     private void getTopBookmarkKGs(){
-        AppController.getApi().getTopBookmarkedKGs(AppController.getInstance().getSessionId(),new Callback<List<KindergartenVM>>() {
+        AppController.getApi().getTopBookmarkedKGs(AppController.getInstance().getSessionId(), new Callback<List<KindergartenVM>>() {
             @Override
             public void success(List<KindergartenVM> vms, Response response) {
                 topBookmarkedKGs.addAll(vms);
-                topBookmarkedKGListAdapter = new TopBookmarkedKGListAdapter(TopSchoolsActivity.this,topBookmarkedKGs);
+                topBookmarkedKGListAdapter = new TopBookmarkedKGListAdapter(TopSchoolsActivity.this, topBookmarkedKGs);
                 topBookmarkedList.setAdapter(topBookmarkedKGListAdapter);
+                setListViewHeightBasedOnChildren(topBookmarkedList);
                 scrollView.fullScroll(View.FOCUS_UP);
             }
 
@@ -200,11 +214,6 @@ public class TopSchoolsActivity extends FragmentActivity {
                 error.printStackTrace();
             }
         });
-    }
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
     }
 
     public void setListViewHeightBasedOnChildren(ListView listView) {
@@ -222,14 +231,27 @@ public class TopSchoolsActivity extends FragmentActivity {
         }
 
         ViewGroup.LayoutParams params = listView.getLayoutParams();
-        params.height=totalHeight + (listView.getDividerHeight() * (listAdapter.getCount()-1));
-
-        System.out.println("hhhh:::"+totalHeight + (listView.getDividerHeight() * (listAdapter.getCount()-1)));
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount()-1));
         listView.setLayoutParams(params);
         listView.requestLayout();
     }
 
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        EasyTracker.getInstance(this).activityStart(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        EasyTracker.getInstance(this).activityStop(this);
+    }
 }
 
 
